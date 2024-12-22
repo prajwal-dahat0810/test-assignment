6d5adca6bc001325e8a6db694b65c4d33fb73ec7
prajwal-dahat0810 <dahatprajwal11@gmail.com>
2024-12-22 05:45:58 +0530

    removed dist files

--- backend/dist/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FRONTEND_URL = void 0;
-exports.FRONTEND_URL = "http://localhost:3000/";
-// export const JWT_SECRET: string = "mysecret";
